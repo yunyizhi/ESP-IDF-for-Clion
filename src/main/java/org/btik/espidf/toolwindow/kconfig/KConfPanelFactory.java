4437b8ab400fdaa989632f28cc067b37e93972ad
yunyizhi <lustreofjade@qq.com>
2025-06-26 17:16:39 +0800

    调整配置项面板布局

--- src/main/java/org/btik/espidf/toolwindow/kconfig/KConfPanelFactory.java
+++ src/main/java/org/btik/espidf/toolwindow/kconfig/KConfPanelFactory.java
@@ -2,6 +2,7 @@ package org.btik.espidf.toolwindow.kconfig;
 
 import com.intellij.openapi.ui.ComboBox;
 import com.intellij.ui.components.JBTextField;
+import com.intellij.util.ui.JBUI;
 import org.btik.espidf.toolwindow.kconfig.model.ConfModel;
 import org.btik.espidf.toolwindow.kconfig.model.KconfigType;
 
@@ -43,74 +44,114 @@ public class KConfPanelFactory {
             KconfigType type = child.getType();
             ItemCreator itemCreator = creators.get(type);
             if (itemCreator == null) {
-                System.out.println(type);
                 continue;
             }
             JComponent comp = itemCreator.create(child);
+            comp.setAlignmentX(Component.LEFT_ALIGNMENT);
             panel.add(comp);
         }
-        panel.add(Box.createVerticalGlue());
         return panel;
     }
 
+    private static void addComponent(JPanel panel, Component component, int gridx, int gridy) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = gridx;
+        gbc.gridy = gridy;
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.insets = JBUI.insets(2, 10, 0, 0); // 内边距
+        panel.add(component, gbc);
+    }
+
     private static JComponent boolItemCreator(ConfModel confModel) {
-        JPanel wrapper = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        JPanel wrapper = new JPanel(new GridBagLayout());
         JCheckBox comp = new JCheckBox(confModel.getTitle());
         comp.setToolTipText(confModel.getHelp());
-        wrapper.add(comp);
-        wrapper.setSize(comp.getPreferredSize());
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = JBUI.insets(2, 10, 0, 0); // 内边距
+        gbc.anchor = GridBagConstraints.WEST;
+        gbc.weightx = 1.0;
+        wrapper.add(comp, gbc);
+
+        wrapper.setMaximumSize(wrapper.getPreferredSize());
         return wrapper;
     }
 
     private static JComponent selectItemCreator(ConfModel confModel) {
-        JPanel wrapper = new JPanel();
-        wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.Y_AXIS));
+        JPanel wrapper = new JPanel(new GridBagLayout());
+
+        JLabel label = new JLabel(confModel.getTitle() + ":");
+        label.setToolTipText(confModel.getHelp());
+
         ComboBox<ConfModel> comboBox = new ComboBox<>();
         List<ConfModel> children = confModel.getChildren();
+        FontMetrics fm = comboBox.getFontMetrics(comboBox.getFont());
+        int width = 200 ;
+        final int paddingAndLogo = 50;
         for (ConfModel child : children) {
             if (child.getType().equals(BOOL)) {
                 comboBox.addItem(child);
+                width = Math.max(fm.stringWidth(child.toString()) + paddingAndLogo, width);
             }
         }
-        JLabel comp = new JLabel(confModel.getTitle() + ":");
-        comp.setToolTipText(confModel.getHelp());
-        wrapper.add(comp);
-        wrapper.add(comboBox);
-        wrapper.add(Box.createVerticalGlue());
+        comboBox.setPreferredSize(new Dimension(width, 30));
+        comboBox.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+
+        addComponent(wrapper, label, 0, 0);
+        addComponent(wrapper, comboBox, 0, 1);
+
+        wrapper.setMaximumSize(wrapper.getPreferredSize());
         return wrapper;
     }
 
     private static JComponent hexCreator(ConfModel confModel) {
-        JPanel wrapper = new JPanel();
-        wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.Y_AXIS));
-        JBTextField jTextField = new JBTextField();
-        JLabel comp = new JLabel(confModel.getTitle() + ":");
-        comp.setToolTipText(confModel.getHelp());
-        wrapper.add(comp);
-        wrapper.add(jTextField);
-        wrapper.add(Box.createVerticalGlue());
+        JPanel wrapper = new JPanel(new GridBagLayout());
+
+        JLabel label = new JLabel(confModel.getTitle() + ":");
+        label.setToolTipText(confModel.getHelp());
+
+        JBTextField textField = new JBTextField();
+        textField.setPreferredSize(new Dimension(200, 30));
+        textField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+
+        addComponent(wrapper, label, 0, 0);
+        addComponent(wrapper, textField, 0, 1);
+
+        wrapper.setMaximumSize(wrapper.getPreferredSize());
         return wrapper;
     }
 
     private static JComponent intCreator(ConfModel confModel) {
-        JPanel wrapper = new JPanel();
-        wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.Y_AXIS));
-        JBTextField jTextField = new JBTextField();
-        JLabel comp = new JLabel(confModel.getTitle() + ":");
-        comp.setToolTipText(confModel.getHelp());
-        wrapper.add(comp);
-        wrapper.add(jTextField);
+        JPanel wrapper = new JPanel(new GridBagLayout());
+
+        JLabel label = new JLabel(confModel.getTitle() + ":");
+        label.setToolTipText(confModel.getHelp());
+
+        JBTextField textField = new JBTextField();
+        textField.setPreferredSize(new Dimension(200, 30));
+        textField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+
+        addComponent(wrapper, label, 0, 0);
+        addComponent(wrapper, textField, 0, 1);
+
+        wrapper.setMaximumSize(wrapper.getPreferredSize());
         return wrapper;
     }
 
     private static JComponent stringCreator(ConfModel confModel) {
-        JPanel wrapper = new JPanel();
-        wrapper.setLayout(new BoxLayout(wrapper, BoxLayout.Y_AXIS));
-        JBTextField jTextField = new JBTextField();
-        JLabel comp = new JLabel(confModel.getTitle() + ":");
-        comp.setToolTipText(confModel.getHelp());
-        wrapper.add(comp);
-        wrapper.add(jTextField);
+        JPanel wrapper = new JPanel(new GridBagLayout());
+
+        JLabel label = new JLabel(confModel.getTitle() + ":");
+        label.setToolTipText(confModel.getHelp());
+
+        JBTextField textField = new JBTextField();
+        textField.setPreferredSize(new Dimension(200, 30));
+        textField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
+
+        addComponent(wrapper, label, 0, 0);
+        addComponent(wrapper, textField, 0, 1);
+
+        wrapper.setMaximumSize(wrapper.getPreferredSize());
         return wrapper;
     }
 }
