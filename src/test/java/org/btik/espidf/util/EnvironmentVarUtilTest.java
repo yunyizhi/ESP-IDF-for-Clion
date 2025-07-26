fe51cf7cc1bdad5c7795c61277b26fdec8676a12
yunyizhi <lustreofjade@qq.com>
2025-07-26 10:21:32 +0800

    修复EnvironmentVarUtilTest 在linux/mac下执行失败

--- src/test/java/org/btik/espidf/util/EnvironmentVarUtilTest.java
+++ src/test/java/org/btik/espidf/util/EnvironmentVarUtilTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -22,6 +23,10 @@ public class EnvironmentVarUtilTest {
             path = Objects.requireNonNull(getClass().getResource("/idf_fake_path")).getPath();
         }
         path = path.replace("%20", " ");
+        // PathEnvironmentVariableUtil 在非windows下需要判断可执行
+        if (!OsUtil.IS_WINDOWS) {
+            Path.of(path).resolve(OsUtil.Const.IDF_EXE).toFile().setExecutable(true);
+        }
         File file = new File(path);
         env = new HashMap<>();
         env.put("PATH", file.getAbsolutePath());
